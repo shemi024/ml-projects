c4bb59a145bdab855bfb7647d56dab471d1992b9
shemi024 <sshameema488@gmail.com>
2022-03-07 15:05:56 +0530

    initial commit

---
+++ carprices/web.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def home():
+     return render_template('home.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    #request.value will help us to read the particular value in the form
+    exp= float(request.values['Age'])
+    #convert the data into the two dimensional data
+    exp=np.reshape(exp,(-1,1))
+    output=model.predict(exp)
+    #select the particular item
+    output=output.item()
+    #rounding the output value to two decimal point
+    output=round(output,2)
+    return render_template ('result.html',prediction_text="your number is.{}".format(output))
+if __name__=='__main__':
+    app.run(port=5000)
+
